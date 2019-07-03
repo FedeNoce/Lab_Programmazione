d70f9f836be021dbab5ab4e584449b297d298373
FedeNoce <47395634+FedeNoce@users.noreply.github.com>
2019-07-03 14:34:44 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,28 +3,28 @@
 
 int main() {
 
-    MatrixTemplate<float> A(3, 2);
-    A.setValue(1, 1, 1.32);
+     MatrixTemplate<int> A(3, 2);
+    A.setValue(1, 1, 4);
     A.setValue(1, 2, 2);
-    A.setValue(2, 1, 1.15);
-    A.setValue(2, 2, 3.47);
-    A.setValue(3, 1, 2.85);
+    A.setValue(2, 1, 6);
+    A.setValue(2, 2, 3);
+    A.setValue(3, 1, 2);
     A.setValue(3, 2, 1);
     A.print();
-    MatrixTemplate<float> B(2, 3);
-    B.setValue(1, 1, 2.89);
-    B.setValue(1, 2, 0.58);
+    MatrixTemplate<int> B(2, 3);
+    B.setValue(1, 1, 2);
+    B.setValue(1, 2, 0);
     B.setValue(1, 3, 5);
-    B.setValue(2, 1, 1.25);
-    B.setValue(2, 2, 1.55);
+    B.setValue(2, 1, 1);
+    B.setValue(2, 2, 5);
     B.setValue(2, 3, 3);
     B.print();
-    MatrixTemplate<float> C = A * B;
+    MatrixTemplate<int> C = A * B;
     C.print();
-    MatrixTemplate<float> D = C.transpose();
+    MatrixTemplate<int> D = C.transpose();
     D.print();
-    MatrixTemplate<float> Dcl = D.selectColumn(1);
+    MatrixTemplate<int> Dcl = D.selectColumn(1);
     Dcl.print();
-    MatrixTemplate<float> Drw = D.selectRow(3);
+    MatrixTemplate<int> Drw = D.selectRow(3);
     Drw.print();
 }
